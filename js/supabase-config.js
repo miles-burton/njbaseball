eb1d624aea4b5db729b7fc13c3f74176f4e87b72
Miles Burton <milesweberburton@gmail.com>
2026-07-14 19:50:13 -0400

    Configure Supabase anon key

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,5 +2,5 @@
 // Fill these with your Supabase project URL and anon public key after running supabase/schema.sql.
 window.NJ_SUPABASE = window.NJ_SUPABASE || {
   url: 'https://jyxclxebnutnxpzrvcgt.supabase.co',
-  anonKey: '',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5eGNseGVibnV0bnhwenJ2Y2d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNjM4NjIsImV4cCI6MjA5OTYzOTg2Mn0.Eisqy7EYwu3mKXdk2Q3nRCJUPhQ2WiqOCaQM4Fk-Qgo',
 };
